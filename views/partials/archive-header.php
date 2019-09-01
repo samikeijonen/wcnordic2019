14fe2912509f60dc2a723ae1b42e3c1dcda379d3
Sami Keijonen <sami.keijonen@foxnet.fi>
2019-09-01 17:41:10 +0300

    Tweak utilitu class names

--- views/partials/archive-header.php
+++ views/partials/archive-header.php
@@ -7,7 +7,7 @@
 
 if ( ! is_front_page() ) : ?>
 
-	<div class="archive-header mx-auto max-width-md pb-8 text-center">
+	<div class="archive-header mx-auto max-w-md pb-8 text-center">
 		<?php
 			the_archive_title( '<h1 class="archive-header__title">', '</h1>' );
 			the_archive_description( '<div class="archive-header__description">', '</div>' );
